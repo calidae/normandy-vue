812847c6c0484dc0e81daad9edc205f876c0480a
Adrià Fontcuberta <adria@calidae.com>
2017-07-15 11:48:13 +0200

    update media showcase variables

--- src/showcase/components/MediaShowcase.vue
+++ src/showcase/components/MediaShowcase.vue
@@ -5,7 +5,7 @@
       description="Place any image- and text-like content side-by-side" />
 
     <media v-bind="{ src, alt }">
-      {{ text }}
+      {{ slot }}
     </media>
 
     <div v-for="(prop, propName) in optionalProps">
@@ -13,11 +13,10 @@
       <hr>
       <pre>prop: <b>{{ propName }}</b></pre>
 
-      <div
-        class="object-wrapper"
-        v-for="value in prop">
+      <div class="object-wrapper" v-for="value in prop">
+        <h3>{{ value }}</h3>
         <media v-bind="{ src, alt, [propName]: value }">
-          {{ text }}
+          {{ slot }}
         </media>
       </div>
 
@@ -40,7 +39,7 @@ export default {
     return {
       src: 'http://via.placeholder.com/350x150',
       alt: 'logo',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, reiciendis fugiat ut voluptate fugit quisquam modi maiores, perspiciatis quasi praesentium ab vel at repellat amet atque? Veniam, officia, sint.',
+      slot: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, reiciendis fugiat ut voluptate fugit quisquam modi maiores, perspiciatis quasi praesentium ab vel at repellat amet atque? Veniam, officia, sint.',
       optionalProps: {
         size: [ 'flush', 'tiny', 'small', 'large', 'huge' ],
         align: [ 'center', 'bottom', 'stretch' ],
