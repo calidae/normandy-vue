6ea146774f345f64705d520894d3eb30897ab67f
Adrià Fontcuberta <adria@calidae.com>
2017-07-15 11:39:09 +0200

    remove old showcase structure

--- src/showcase/components/grid.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: 'Grid',
-  name: 'grid',
-  description: 'Grid-like layout system, the Flexbox way.',
-  requiredProps: {},
-  props: {}
-}
--- src/showcase/components/media.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: 'Media',
-  name: 'media'
-}
