4e01bd2009e05bf86f10c5541511d9d9263a1932
Adrià Fontcuberta <adria@calidae.com>
2017-07-24 08:45:56 +0200

    rename spec for test

--- src/components/Media/Media.test.js
+++ src/components/Media/Media.test.js
@@ -13,23 +13,23 @@ describe('Media.vue', () => {
   }))
 
   it('renders correct nodes when required props are provided', () => {
-    const vm = mount(Media, { propsData })
-    expect(vm.hasClass('o-media')).toBe(true)
-    expect(vm.find('.o-media__body')).toBeTruthy()
-    expect(vm.find('.o-media__img')).toBeTruthy()
+    const wrapper = mount(Media, { propsData })
+    expect(wrapper.hasClass('o-media')).toBe(true)
+    expect(wrapper.find('.o-media__body')).toBeTruthy()
+    expect(wrapper.find('.o-media__img')).toBeTruthy()
   })
 
   it('renders slot content when defined', () => {
-    const vm = mount(Media, { propsData, slots: { default: Dummy } })
-    expect(vm.first('.Dummy')).toBeTruthy()
+    const wrapper = mount(Media, { propsData, slots: { default: Dummy } })
+    expect(wrapper.first('.Dummy')).toBeTruthy()
   })
 
   it('adds size class when size prop is provided', () => {
     const values = ['flush', 'tiny', 'small', 'large', 'huge']
 
     values.forEach((size) => {
-      const vm = mount(Media, { propsData: { ...propsData, size } })
-      expect(vm.hasClass(`o-media--${size}`)).toBe(true)
+      const wrapper = mount(Media, { propsData: { ...propsData, size } })
+      expect(wrapper.hasClass(`o-media--${size}`)).toBe(true)
     })
   })
 
@@ -37,34 +37,34 @@ describe('Media.vue', () => {
     const values = ['center', 'bottom', 'stretch']
 
     values.forEach((align) => {
-      const vm = mount(Media, { propsData: { ...propsData, align } })
-      expect(vm.hasClass(`o-media--${align}`)).toBe(true)
+      const wrapper = mount(Media, { propsData: { ...propsData, align } })
+      expect(wrapper.hasClass(`o-media--${align}`)).toBe(true)
     })
   })
 
   it('adds reverse class when reverse prop is provided', () => {
-    const vm = mount(Media, { propsData: { ...propsData, reverse: true } })
-    expect(vm.hasClass('o-media--reverse')).toBe(true)
+    const wrapper = mount(Media, { propsData: { ...propsData, reverse: true } })
+    expect(wrapper.hasClass('o-media--reverse')).toBe(true)
   })
 
   it('adds all optional classes when all props are provided', () => {
-    const vm = mount(Media, {
+    const wrapper = mount(Media, {
       propsData: { ...propsData, size: 'small', align: 'stretch', reverse: true }
     })
-    expect(vm.hasClass('o-media--reverse')).toBe(true)
-    expect(vm.hasClass('o-media--small')).toBe(true)
-    expect(vm.hasClass('o-media--stretch')).toBe(true)
+    expect(wrapper.hasClass('o-media--reverse')).toBe(true)
+    expect(wrapper.hasClass('o-media--small')).toBe(true)
+    expect(wrapper.hasClass('o-media--stretch')).toBe(true)
   })
 
   it('checks that changing size prop changes its class', () => {
-    const vm = mount(Media, { propsData: { ...propsData, size: 'large' } })
-    expect(vm.hasClass('o-media--large')).toBe(true)
-    expect(vm.hasClass('o-media--small')).toBe(false)
+    const wrapper = mount(Media, { propsData: { ...propsData, size: 'large' } })
+    expect(wrapper.hasClass('o-media--large')).toBe(true)
+    expect(wrapper.hasClass('o-media--small')).toBe(false)
 
-    vm.setProps({ size: 'small' })
+    wrapper.setProps({ size: 'small' })
 
-    expect(vm.hasClass('o-media--small')).toBe(true)
-    expect(vm.hasClass('o-media--large')).toBe(false)
+    expect(wrapper.hasClass('o-media--small')).toBe(true)
+    expect(wrapper.hasClass('o-media--large')).toBe(false)
   })
 
   it('breaks when required props are not provided', () => {
