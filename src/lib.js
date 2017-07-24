5769e0d8a2e2a7a51cb7ac71ad1994ec73fe584e
Adrià Fontcuberta <adria@calidae.com>
2017-07-24 20:38:43 +0200

    publish block element

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,10 @@
 import Media from '@/components/Media'
+import Block from '@/components/Block'
 import { Grid, GridItem } from '@/components/Grid'
 
 export {
   Media,
   Grid,
-  GridItem
+  GridItem,
+  Block
 }
