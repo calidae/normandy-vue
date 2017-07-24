1952fd83bfd5db29be0d0ab0a32951c0e756f9eb
Adrià Fontcuberta <adria@calidae.com>
2017-07-24 20:37:37 +0200

    block files structure

---
+++ src/components/Block/Block.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="o-block">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  data () {
+    return {}
+  },
+  props: {}
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/showcase/components/BlockShowcase.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <component-header
+      title="Block"
+      description="Stacked image-with-text object. A simple abstraction to cover a very commonly occurring design pattern." />
+
+  </div>
+</template>
+
+<script>
+import ComponentHeader from '../ComponentHeader'
+import Block from '@/components/Block'
+
+export default {
+  name: 'block-showcase',
+  components: {
+    [ComponentHeader.name]: ComponentHeader,
+    [Block.name]: Block
+  },
+  data () {
+    return {}
+  }
+}
+</script>
