9b63e93f03f47455fe1a947491743079324f5cec
Adrià Fontcuberta <adria@calidae.com>
2017-07-28 10:29:49 +0200

    update build configurations to match vue-library-template

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -62,6 +62,14 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
       }
     ]
   }
