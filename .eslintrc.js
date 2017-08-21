d0d91aaa8c6da1ffcae239530964f3d82092e4fa
Adrià Fontcuberta <adria@calidae.com>
2017-08-21 22:43:00 +0200

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: "babel-eslint",
     sourceType: 'module'
   },
   env: {
@@ -11,11 +11,17 @@ module.exports = {
     jest: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'standard',
+    'plugin:vue/recommended'
+  ],
   // required to lint *.vue files
   plugins: [
     'html'
   ],
+  settings: {
+    "html/html-extensions": [".html"], // don't include .vue
+  },
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
@@ -25,6 +31,10 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-return-assign': 0,
+    'vue/no-shared-component-data': 'error',
+    'vue/no-side-effects-in-computed-properties': 'error',
+    'vue/v-bind-style': 'error',
+    'vue/v-on-style': 'error',
   },
   "globals": {
     "expect": true
