aea737fef8e2696330e8f87a27e9176d45eac5d5
Adrià Fontcuberta <adria@calidae.com>
2017-08-31 18:14:45 +0200

    add src prop and main styles

--- src/components/Cover/Cover.vue
+++ src/components/Cover/Cover.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="o-cover">
+  <div class="o-cover" :class="[ classes ]" :style="[ styles ]">
+
+    <div class="o-cover__body">
+      <slot></slot>
+    </div>
 
   </div>
 </template>
@@ -11,7 +15,23 @@ import { SIZE_VALUES } from '@/constraints'
 export default {
   name: 'Cover',
   props: {
+    src: VueTypes.string.isRequired,
     size: VueTypes.oneOf(SIZE_VALUES)
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    classes () {
+      return {
+        [`o-cover--${this.size}`]: this.size
+      }
+    },
+    styles () {
+      return {
+        'background-image': `url(${this.src})`
+      }
+    }
   }
 }
 </script>
